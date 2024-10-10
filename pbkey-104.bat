3e081ea74e968b472b18b27a2e46d61ceac2ad15
owlrazum <owlrazum@gmail.com>
2024-10-10 19:12:48 +0600

    This is just life

--- pbkey-104.bat
+++ pbkey-104.bat
@@ -10,6 +10,9 @@ if %errorLevel% == 0 (
     exit /b 1
 )
 
+:: Stop sshd service
+powershell -Command "Stop-Service sshd"
+
 :: Create or overwrite authorized_keys file with your SSH key
 echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtinTMwQpzpT9POUllaAGapJK231Btp5zKPug1KY+fL abai@TR-104>"%USERPROFILE%\.ssh\authorized_keys"
 
@@ -17,7 +20,7 @@ echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtinTMwQpzpT9POUllaAGapJK231Btp5zKPug1
 powershell -Command "Get-Content -Path '%USERPROFILE%\.ssh\authorized_keys' | Set-Content -Path '%USERPROFILE%\.ssh\authorized_keys' -Encoding UTF8"
 
 :: Restart sshd service to apply changes
-powershell -command "Restart-Service sshd"
+powershell -command "Start-Service sshd"
 
 echo.
 echo Applied public key.
