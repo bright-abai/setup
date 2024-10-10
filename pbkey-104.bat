8c9e595110c38af429f5880b3e3752fe92a03eb6
owlrazum <owlrazum@gmail.com>
2024-10-10 19:38:28 +0600

    add russian for администраторы

--- pbkey-104.bat
+++ pbkey-104.bat
@@ -18,10 +18,10 @@ echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtinTMwQpzpT9POUllaAGapJK231Btp5zKPug1
 powershell -Command "Get-Content -Path '%USERPROFILE%\.ssh\authorized_keys' | Set-Content -Path '%USERPROFILE%\.ssh\authorized_keys' -Encoding UTF8"
 
 :: Remove all permissions and disable inheritance on .ssh directory
-icacls "%USERPROFILE%\.ssh" /inheritance:r /remove:g "Administrators" /remove:g "Users" /grant:r "%USERNAME%:F" "SYSTEM:F"
+icacls "%USERPROFILE%\.ssh" /inheritance:r /remove:g "Administrators" /remove:g "Администраторы" /grant:r "%USERNAME%:F" "SYSTEM:F"
 
 :: Remove all permissions and disable inheritance on authorized_keys file
-icacls "%USERPROFILE%\.ssh\authorized_keys" /inheritance:r /remove:g "Administrators" /remove:g "Users" /grant:r "%USERNAME%:F" "SYSTEM:F"
+icacls "%USERPROFILE%\.ssh\authorized_keys" /inheritance:r /remove:g "Administrators" /remove:g "Администраторы" /grant:r "%USERNAME%:F" "SYSTEM:F"
 
 :: Start sshd service
 powershell -command "Start-Service sshd"
