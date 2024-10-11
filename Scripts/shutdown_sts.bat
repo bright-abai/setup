723082966e73d27a11c919d4bbc2f47f8899de22
owlrazum <owlrazum@gmail.com>
2024-10-11 17:06:52 +0600

    shutdown script 104

--- Scripts/shutdown_sts.bat
+++ Scripts/shutdown_sts.bat
@@ -1,15 +1,13 @@
 @echo off
-setlocal enabledelayedexpansion
-
-for /L %%i in (1,1,20) do (
-    set "hostname=ST-104-0%%i"
-    if %%i gtr 9 set "hostname=ST-104-%%i"
+for /l %%i in (1,1,20) do (
+    setlocal enabledelayedexpansion
+    if %%i lss 10 (set "suffix=0%%i") else (set "suffix=%%i")
+    set "address=Admin@ST-104-!suffix!"
     
-    ssh Admin@!hostname! "shutdown /s /t 0"
-    if errorlevel 1 (
-        echo Failed to connect or execute shutdown on !hostname!
-    ) else (
-        echo Successfully shut down !hostname!
-    )
+    echo Starting connection to !address! in parallel...
+    
+    start cmd /c ssh !address! "shutdown -s -t 0"
+    
+    endlocal
 )
 pause
