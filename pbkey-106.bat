5e4a03f88bcbc324ab64dc30cb9aaf7d77148aab
Abai <abai@bright.kg>
2024-10-14 16:21:17 +0600

    Create pbkey-106.bat

---
+++ pbkey-106.bat
@@ -0,0 +1,21 @@
+@echo off
+
+:: Check if running as administrator
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Please run this script as an administrator.
+    pause
+    exit /b 1
+)
+
+:: Create or overwrite authorized_keys file with your SSH key
+echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDRjFCd+93HllSalNU7uXz0TyXgqi2C1Ezyu0RGSW3AL abai@TR-106 > "%USERPROFILE%\.ssh\104pbkey.txt"
+
+:: Ensure the file is saved with UTF-8 encoding
+powershell -Command "Get-Content -Path '%USERPROFILE%\.ssh\authorized_keys' | Set-Content -Path '%USERPROFILE%\.ssh\authorized_keys' -Encoding UTF8"
+
+powershell -Command "code '%USERPROFILE%\.ssh'"
+
+echo.
+echo Applied public key with correct permissions.
+pause
