2eb9d2cc13af2a69a190cec37ef9ba7628a55f6b
Abai <abai@bright.kg>
2024-10-23 20:08:16 +0600

    Update wallpaper.ps1

--- Scripts/wallpaper.ps1
+++ Scripts/wallpaper.ps1
@@ -1,27 +1,3 @@
-param(
-    [Parameter(Mandatory = $true)]
-    [string]$img
-)
-
-$srcUsrImg = Join-Path -Path $PSScriptRoot -ChildPath "numbers\$img.jpg"
-$srcAdmImg = Join-Path -Path $PSScriptRoot -ChildPath "Bright.jpg"
-$usrImg = "C:\Users\Wallpapers\$img.jpg"
-$admImg = "C:\Users\Wallpapers\Bright.jpg"
-Copy-Item -Path $srcUsrImg -Destination $usrImg -Force
-Copy-Item -Path $srcAdmImg -Destination $admImg -Force
-
-Write-Host "Images copied to C:\Users\Wallpapers."
-
-$usr = (Get-LocalUser -Name $ "ST-20").SID
-$adm = (Get-LocalUser -Name $ "Admin").SID
-
-Edit-Registry -username "ST-20" -path "Registry::HKEY_USERS\$usr\software\microsoft\windows\currentVersion\policies\system" -prop "Wallpaper" -value $usrImg -type  "String"
-Edit-Registry -username "Admin" -path "Registry::HKEY_USERS\$usr\software\microsoft\windows\currentVersion\policies\system" -prop "WallpaperStyle" -value "3" -type  "String"
-
-Edit-Registry -username "ST-20" -path "Registry::HKEY_USERS\$adm\software\microsoft\windows\currentVersion\policies\system" -prop "Wallpaper" -value $admImg -type  "String"
-Edit-Registry -username "Admin" -path "Registry::HKEY_USERS\$adm\software\microsoft\windows\currentVersion\policies\system" -prop "WallpaperStyle" -value "3" -type  "String"
-
-
 function Edit-Registry {
     param (
         [string]$username,
@@ -43,3 +19,34 @@ function Edit-Registry {
 
     Write-Host "Completed"
 }
+
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$img
+)
+
+$wallpapers = "C:\Users\Wallpapers"
+$srcUsrImg = Join-Path -Path $PSScriptRoot -ChildPath "numbers\$img.jpg"
+$srcAdmImg = Join-Path -Path $PSScriptRoot -ChildPath "Bright.jpg"
+$usrImg = "C:\Users\Wallpapers\$img.jpg"
+$admImg = "C:\Users\Wallpapers\Bright.jpg"
+
+if (-not (Test-Path -Path $wallpapers)) {
+    New-Item -Path $wallpapers -ItemType Directory -Force
+    Write-Host "Created destination directory: $wallpapers"
+}
+
+Copy-Item -Path $srcUsrImg -Destination $usrImg -Force
+Copy-Item -Path $srcAdmImg -Destination $admImg -Force
+
+Write-Host "Images copied to C:\Users\Wallpapers."
+
+$usr = (Get-LocalUser -Name "ST-20").SID
+$adm = (Get-LocalUser -Name "Admin").SID
+
+Edit-Registry -username "ST-20" -path "Registry::HKEY_USERS\$usr\software\microsoft\windows\currentVersion\policies\system" -prop "Wallpaper" -value $usrImg -type  "String"
+Edit-Registry -username "Admin" -path "Registry::HKEY_USERS\$usr\software\microsoft\windows\currentVersion\policies\system" -prop "WallpaperStyle" -value "3" -type  "String"
+
+Edit-Registry -username "ST-20" -path "Registry::HKEY_USERS\$adm\software\microsoft\windows\currentVersion\policies\system" -prop "Wallpaper" -value $admImg -type  "String"
+Edit-Registry -username "Admin" -path "Registry::HKEY_USERS\$adm\software\microsoft\windows\currentVersion\policies\system" -prop "WallpaperStyle" -value "3" -type  "String"
+
