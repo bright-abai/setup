c312679f8d6141aeeb75bc24b2ece816fbca3f3e
Owlrazum <owlrazum@gmail.com>
2024-10-26 11:22:33 +0600

    home

---
+++ Scripts/pwsh/Edit-Registry.ps1
@@ -0,0 +1,52 @@
+param (
+    [Parameter(Mandatory = $true)][string]$username
+    [Parameter(Mandatory = $true)][string[]]$path
+    [Parameter(Mandatory = $true)][string[]]$prop
+    [Parameter(Mandatory = $true)][string[]]$type
+    [Parameter(Mandatory = $true)][string[]]$val
+)
+
+function Load-Username {
+    param (
+        [string]$username
+    )
+
+    $sid = (New-Object System.Security.Principal.NTAccount($username)).Translate([System.Security.Principal.SecurityIdentifier]).Value
+    $loaded = $false
+
+    if (-not (Test-Path Registry::HKEY_USERS\$sid)) {
+        reg load "HKEY_USERS\$username" "C:\Users\$username\NTUSER.DAT"
+        $sid = $username
+        $loaded = true
+    } 
+
+    return @($sid, $loaded)
+}
+
+function Edit-Registry {
+    param (
+        [string]$path,
+        [string]$prop,
+        [string]$value,
+        [string]$type
+    )
+
+    try {
+        $current = Get-ItemProperty -Path $path -Name $prop -ErrorAction Stop
+        Set-ItemProperty -Path $path -Name $prop -Value $value
+        Write-Host "Property $prop at $path updated for $username with $value, replacing $($current.$prop)"
+    
+    } catch {
+        New-ItemProperty -Path $path -Name $prop -Value $value -PropertyType $type
+        Write-Host "Property $prop at $path created for $username with $value."
+    }
+}
+
+$sid = Load-Username -username $username
+for ($i = 0; $i -lt $path.Length; $i++) {
+    Edit-Registry -path $($path[$i]) -prop $($prop[$i]) -value $($val[$i]) -type $($type[$i])"
+}
+
+if ($sid[1]) {
+    reg unload
+}
