f7fae9831cfcb3244c88703455093d0c30d933c6
Owlrazum <owlrazum@gmail.com>
2024-10-27 12:57:18 +0600

    dis

--- Scripts/pwsh/Disable-ChangeSettings.ps1
+++ Scripts/pwsh/Disable-ChangeSettings.ps1
@@ -5,16 +5,59 @@ param (
 . "$PSScriptRoot\LibEditRegistries.ps1"
 
 $paths = @(
-    "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\Settings",
+    "HKLM:\software\microsoft\policymanager\default\settings"                  # 01.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\system"        # 02.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\system"        # 03.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\system"        # 04.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\explorer"      # 05.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\explorer"      # 06.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\explorer"      # 07.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\explorer"      # 08.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\explorer"      # 09.
+    , "HKCU:\software\microsoft\windows\currentversion\policies\activedesktop" # 10.
+    , "HKCU:\software\policies\microsoft\windows\personalization"              # 11.
+
 )
 $props = @(
-    "AllowLanguage",
+    "AllowLanguage"              # 01. settings
+    , "NoDispAppearancePage"     # 02. system
+    , "NoDispBackgroundPage"     # 03. system
+    , "NoColorChoice"            # 04. system
+    , "NoThemesTab"              # 05. explorer
+    , "NoDesktop"                # 06. explorer
+    , "NoCloseDragDropBands"     # 07. explorer Toolbar
+    , "NoMovingBands"            # 08. explorer Toolbar
+    , "NoActiveDesktop"          # 09. explorer (active desktop is not needed featuer, but no changing wallpaper is located under it)
+    , "NoChangingWallPaper"      # 10. activedesktop
+    , "NoChangingMousePointers"  # 11. personalization
+
 )
 $types = @(
-    "DWord",
+    "DWord"     # 01. settings
+    , "DWord"   # 02. system
+    , "DWord"   # 03. system
+    , "DWord"   # 04. system
+    , "DWord"   # 05. explorer
+    , "DWord"   # 06. explorer
+    , "DWord"   # 07. explorer
+    , "DWord"   # 08. explorer
+    , "DWord"   # 09. explorer
+    , "DWord"   # 10. activedesktop
+    , "DWord"   # 11. personalization
+
 )
 $vals  = @(
-    0,
+    1   # 01. AllowLanguage settings
+    , 0 # 02. system
+    , 0 # 03. system
+    , 0 # 04. system
+    , 0 # 05. explorer
+    , 0 # 06. explorer
+    , 0 # 07. explorer
+    , 0 # 08. explorer
+    , 0 # 09. explorer
+    , 0 # 10. activedesktop
+    , 0 # 11. personalization
 )
 
 EditRegitries -username $username -path $paths -prop $props -type $types -val $vals
