cbe4492f0b7c9f1a44e05be6e6d563cddcd3cd01
Abai <abai@bright.kg>
2024-10-28 09:50:44 +0600

    Update Apply-Wallpaper.ps1

--- Scripts/pwsh/Apply-Wallpaper.ps1
+++ Scripts/pwsh/Apply-Wallpaper.ps1
@@ -1,32 +1,29 @@
-param (
-    [Parameter(Mandatory = $true)][string]$userIndex
-)
-
 $wallpapersFolder = "C:\Wallpapers"
 $controlFolder = "C:\Control"
-$student = "ST-{0:D2}" -f $userIndex
+$studentName = (Get-LocalUser | Where-Object { $_.Name -like 'ST-*' } | Select-Object -ExpandProperty Name)
+$studentNumber = $studentName[-2..-1] -join ''
 
-. "$PSScriptRoot\LibEditRegistries.ps1"
+. "$PSScriptRoot\LibEdit-Registries.ps1"
 
 function Set-Wallpaper {
     param (
-        [string]$username
-        [string]$sourceImage
+        [string]$username,
+        [string]$sourceImage,
         [string]$destinationImage
     )
 
     Copy-Item -Path $sourceImage -Destination $destinationImage -Force
 
     $regs = @(
-        @("HKU:\$sid\software\microsoft\windows\currentVersion\policies\system", "Wallpaper"     , "String", $destinationImage),
-        @("HKU:\$sid\software\microsoft\windows\currentVersion\policies\system", "WallpaperStyle", "DWord" , 3)
+        @("Registry::HKEY_USERS:\$sid\software\microsoft\windows\currentVersion\policies\system", "Wallpaper"     , "String", $destinationImage), #HKU is not found somehow
+        @("Registry::HKEY_USERS:\$sid\software\microsoft\windows\currentVersion\policies\system", "WallpaperStyle", "DWord" , 3)
     )
-    EditRegitries -Username $username -Regs $regs
+    Edit-Registries -Username $username -Regs $regs
 }
 
-$userSource  = [System.IO.Path]::Combine($controlFolder, "numbers", "$userIndex.jpg")
+$userSource  = [System.IO.Path]::Combine($controlFolder, "numbers", "$studentNumber.jpg")
 $adminSource = [System.IO.Path]::Combine($controlFolder, "bright.jpg")
-$userDestination  = [System.IO.Path]::Combine($wallpapersFolder, "$student.jpg")
+$userDestination  = [System.IO.Path]::Combine($wallpapersFolder, "$studentName.jpg")
 $adminDestination = [System.IO.Path]::Combine($wallpapersFolder, "admin.jpg")
 
 Set-Wallpaper -UserName $student -SourceImage $userSource -DestinationImage $userDestination
@@ -36,9 +33,9 @@ $path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
 $props = @("LockScreenImagePath", "LockScreenImageUrl", "LockScreenImageStatus")
 $types = @("String", "String", "DWord")
 $vals = @($adminDestination, "$adminDestination", 1)
-EditRegitry -Path $path -Prop $props[0]-Type $types[0] -Value $vals[0]
-EditRegitry -Path $path -Prop $props[1]-Type $types[1] -Value $vals[1]
-EditRegitry -Path $path -Prop $props[2]-Type $types[2] -Value $vals[2]
+Edit-Registry -Path $path -Prop $props[0]-Type $types[0] -Value $vals[0]
+Edit-Registry -Path $path -Prop $props[1]-Type $types[1] -Value $vals[1]
+Edit-Registry -Path $path -Prop $props[2]-Type $types[2] -Value $vals[2]
 
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
