f86b7036f0eb45807754df3a49fdb9317ecac101
owlrazum_mac <owlrazum@gmail.com>
2024-11-03 16:09:11 +0600

    fix allow language

--- Enable-ChangeSettings.ps1
+++ Enable-ChangeSettings.ps1
@@ -1,8 +1,15 @@
+# Check if running as administrator
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as an administrator."
+    Pause
+    exit 1
+}
+
 . "$PSScriptRoot\LibEdit-Registries.ps1"
 
 $regsMachine = @(
-    @("HKLM:\software\microsoft\policymanager\default\settings"                , "AllowLanguage"          , "DWord", "1"),
-    @("HKLM:\software\policies\microsoft\windows\personalization"              , "NoChangingLockScreen"   , "DWord", "0")
+    @("HKLM:\software\microsoft\policymanager\default\settings\allowlanguage", "value"               , "DWord", "1"),
+    @("HKLM:\software\policies\microsoft\windows\personalization"            , "NoChangingLockScreen", "DWord", "0")
 )
 
 $regsUser = @(
