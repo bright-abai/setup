787291b53bb71033c67829ccc4091fef633040ba
owlrazum <owlrazum@gmail.com>
2024-11-23 16:07:10 +0600

    kj

--- scripts/Set-English.ps1
+++ scripts/Set-English.ps1
@@ -1,20 +1,29 @@
 $studentName = (Get-LocalUser | Where-Object { $_.Name -like 'ST-*' } | Select-Object -ExpandProperty Name)
-
-# $english = Get-InstalledLanguage | Where-Object { $_.LanguageId -eq "en-US"}
-# if ($english.Count -eq 0) {
-#     Install-Language en-US
-# }
-
-Set-WinSystemLocale -SystemLocale en-US
-Set-SystemPreferredUILanguage en-US
-Set-WinUILanguageOverride
-Set-WinDefaultInputMethodOverride -InputTip "0409:00000409"
-
 . "$PSScriptRoot\LibEdit-Registries.ps1"
 
 $regs = @(
     @("control panel\international", "Locale"    , "String", "0409"),
-    @("control panel\international", "LocaleName", "String", "en-US")
+    @("control panel\international", "LocaleName", "String", "en-US"),
+    @("Software\Policies\Microsoft\Control Panel\Desktop", "MultiUILanguageID", "String", "00000409"),
+    @("Software\Policies\Microsoft\Control Panel\Desktop", "PreferredUILanguages", "String", "en-US")
 )
+
 Edit-RegistriesUser -Username $studentName -Regs $regs
 Edit-RegistriesUser -Username "Admin" -Regs $regs
+
+$regs = @(
+    @("HKLM:\Software\Policies\Microsoft\MUI\Settings", "MachineUILock", "DWord", 1), 
+    @("HKLM:\system\CurrentControlSet\Control\Nls\Language", "InstallLanguage", "String", "0409"),
+    @("HKLM:\system\CurrentControlSet\Control\Nls\Language", "InstallLanguageFallback", "String", "en-US")
+)
+Edit-RegistriesMachine -Regs $regs
+
+# $en = "en-US"
+# Set-WinUILanguageOverride -Language $en
+# Set-WinDefaultInputMethodOverride -InputTip "0409:00000409"
+# Set-WinUILanguageOverride -Language $en
+
+# Set-WinSystemLocale $en
+# Set-WinUserLanguageList $en -Force
+
+# Set-SystemPreferredUILanguage en-US -PassThru
