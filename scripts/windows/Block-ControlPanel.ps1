915137f3262e707a7158ac4e7d01f048c91b2052
Abai <bright.owlrazum@gmail.com>
2026-01-23 17:57:14 +0600

    Create Block-ControlPanel.ps1

---
+++ scripts/windows/Block-ControlPanel.ps1
@@ -0,0 +1,11 @@
+# Check if running as administrator
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as an administrator."
+    Pause
+    exit 1
+}
+
+. "$PSScriptRoot\LibEdit-Registries.ps1"
+
+# The reason is to allow teacher access to the control panel
+Edit-RegistriesUser -UserName "student" -Regs @(@("software\microsoft\windows\currentversion\policies\explorer", "NoControlPanel", "DWord", "1"))
